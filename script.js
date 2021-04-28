42bb0b85ee2e10b6e539353206cc8fdfbb54221d
Kayleen Carido <kvcarido@gmail.com>
2021-04-27 22:57:59 -0700

    setup components, started JS functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(`hello 🌎`);
+const userInput = document.querySelector(`#user-input`);
+const guessBtn = document.querySelector(`#guess-btn`);
+const pokemonField = document.querySelector(`#pokemon-field`);
+
+guessBtn.addEventListener(`click`, (e) => {
+    e.preventDefault();
+
+    let guess = userInput.value;
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/eevee`)
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
